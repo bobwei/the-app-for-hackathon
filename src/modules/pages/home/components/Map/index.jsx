5b5dcc93dacaea3bc9241c207140b4719f0de1b2
victor wang <iwantooxxoox@gmail.com>
2017-03-11 23:16:34 +0800

    be able to show marker on map

--- src/modules/pages/home/components/Map/index.jsx
+++ src/modules/pages/home/components/Map/index.jsx
@@ -2,13 +2,42 @@
 import React from 'react';
 import compose from 'recompose/compose';
 
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
+const generateInitialMarkers = () => {
+  // const southWest = new google.maps.LatLng(-31.203405, 125.244141);
+  // const northEast = new google.maps.LatLng(-25.363882, 131.044922);
+
+  // const lngSpan = northEast.lng() - southWest.lng();
+  // const latSpan = northEast.lat() - southWest.lat();
+
+  const markers = [
+    { lat: 25.04, lng: 121.52 },
+
+  ];
+  // for (let i = 0; i < 5; i++) {
+  //   const position = new google.maps.LatLng(
+  //     southWest.lat() + latSpan * Math.random(),
+  //     southWest.lng() + lngSpan * Math.random()
+  //   );
+  //   markers.push({
+  //     position,
+  //     content: `This is the secret message`.split(` `)[i],
+  //     showInfo: false,
+  //   });
+  // }
+  return markers;
+};
+generateInitialMarkers();
 const SimpleMapExampleGoogleMap = withGoogleMap(props => (
   <GoogleMap
     defaultZoom={8}
     {...props}
-  />
+  >
+    <Marker
+      position={{ lat: 25.04, lng: 121.52 }}
+    />
+  </GoogleMap>
 ));
 
 const MyMap = ({ lat, lng }) => (
