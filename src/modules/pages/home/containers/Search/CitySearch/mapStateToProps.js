42fbe79bbba4cc70e3d73895c39dfa2fe4c5b260
bobwei <bob61016@gmail.com>
2017-03-12 15:57:09 +0800

    add filter

--- src/modules/pages/home/containers/Search/CitySearch/mapStateToProps.js
+++ src/modules/pages/home/containers/Search/CitySearch/mapStateToProps.js
@@ -1,5 +1,10 @@
-const mapStateToProps = state => ({
-  photos: state.photos.list,
+import R from 'ramda';
+
+const mapStateToProps = (state, { location: { query: { selectedCity = '' } } }) => ({
+  photos: R.filter(R.compose(
+    R.contains(selectedCity),
+    R.prop('name'),
+  ))(state.photos.list),
 });
 
 export default mapStateToProps;
